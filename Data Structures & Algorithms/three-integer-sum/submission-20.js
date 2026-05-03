183f4b0a26693c3ea825b3b1e9a196f7a7eee63f
Zachary Springer <116612584+lordbaldwin1@users.noreply.github.com>
2026-05-02 18:05:51 -0700

    Add: three-integer-sum - submission-20

---
+++ Data Structures & Algorithms/three-integer-sum/submission-20.js
@@ -0,0 +1,34 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @return {number[][]}
+     */
+    threeSum(nums) {
+        const res = [];
+        nums.sort((a, b) => a - b);
+
+        for (let i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) break;
+            if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+            let l = i + 1;
+            let r = nums.length -1;
+            while (l < r) {
+                const sum = nums[i] + nums[l] + nums[r];
+                if (sum === 0) {
+                    res.push([nums[i], nums[l], nums[r]]);
+
+                    l++;
+                    while (l < r && nums[l] === nums[l - 1]) {
+                        l++;
+                    }
+                } else if (sum < 0) {
+                    l++;
+                } else {
+                    r--;
+                }
+            }
+        }
+        return res;
+    }
+}
