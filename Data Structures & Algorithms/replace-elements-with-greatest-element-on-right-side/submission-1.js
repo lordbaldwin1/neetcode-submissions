3287ca8ea6dabb7396f8120c60a0d62ac83247b2
Zachary Springer <116612584+lordbaldwin1@users.noreply.github.com>
2026-06-18 17:15:42 -0700

    Add: replace-elements-with-greatest-element-on-right-side - submission-1

---
+++ Data Structures & Algorithms/replace-elements-with-greatest-element-on-right-side/submission-1.js
@@ -0,0 +1,17 @@
+class Solution {
+    /**
+     * @param {number[]} arr
+     * @return {number[]}
+     */
+    replaceElements(arr) {
+        let max = -1;
+
+        for (let i = arr.length - 1; i >= 0; i--) {
+            const cur = arr[i];
+            arr[i] = max;
+            max = Math.max(max, cur);
+        }
+
+        return arr;
+    }
+}
